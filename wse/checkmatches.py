6d0f158a9aecee8f251b70515e86d07adc9bad00
Soren Rasmussen <s.c.rasmussen@gmail.com>
2018-09-23 17:28:10 +0100

    update

--- wse/checkmatches.py
+++ wse/checkmatches.py
@@ -1,5 +1,4 @@
 INTERWEBZ=False
-
 #
 # checkmatches.py
 # A non-spoiler top prowrestling match finder
@@ -9,6 +8,8 @@ INTERWEBZ=False
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from random import randint
+from datetime import datetime as dt
+from dateutil.parser import parse
 import pandas as pd
 
 # List name
@@ -16,70 +17,20 @@ import pandas as pd
 #  |-check a movie against allflicks.net
 
 
+def suffix(d):
+    return 'th' if 11<=d<=13 else {1:'st',2:'nd',3:'rd'}.get(d%10, 'th')
 
+def sortTopMatches(df):
+  df.to_csv("topMatches.csv", index=False, header=False)
 
-def initListList():
-    print("Create listList.txt")
-    listList=[]
-    # from 1 to 8
-    for i in range(1,9):
-        ICHECKURL="https://www.icheckmovies.com/lists/?page="+str(i)+"&tags=user:icheckmovies"
-        req = urlopen(ICHECKURL);
-        soup = BeautifulSoup(req,"lxml")
-        titleList = soup.findAll(class_="title")
-        for title in titleList[:-1]:
-            listList.append(title["href"])
-
-    f = open("listList.txt", 'w')
-    for name in listList:
-        f.write("%s\n"%name[7:-1])
-    f.close()
-
-def readListList():
-    listList = []
-    f = open("listList.txt")
-    listList = f.read().splitlines()
-    f.close()
-    return listList
-
-def chooseList(listList):
-    print("Here is the list:")
-    for i, name in enumerate(listList):
-        print(i, name)
-    return int(input("\nWhich playlist would you like to check? "))
-
-def getTitleList(listName):
-    ICHECKURL="https://www.icheckmovies.com/lists/"+listName
-    req = urlopen(ICHECKURL);
-    soup = BeautifulSoup(req,"lxml")
-    titles = soup.findAll(class_="optionIcon optionIMDB external")
-    titleList = []
-    for title in titles:
-        name=title["title"]
-        titleList.append(name[6:-12])
-    return titleList
+def sortByDate(df):
+  df["DATE"] =pd.to_datetime(df.DATE)
+  df.sort_values('DATE',inplace=True)
+  # print(df)
+  df.to_csv("sortByDate.csv", index=False, header=False)
 
 
 print("Start")
-
-# # Read name of movie lists from file or generate file from website
-# readList=1
-# if readList:
-#     listList = readListList()
-# else:
-#     initListList()
-
-# # Choose list
-# #i = chooseList(listList)
-# i=59
-# print(listList[i])
-
-# # titleList = getTitleList(listList[i])
-
-# movie = "Jaws"
-
-
-# SEARCH FOR A SPECIFIC MOVIE
 if (INTERWEBZ):
   SEARCHURL="http://www.profightdb.com/top-rated-matches.html"
   req = urlopen(SEARCHURL);
@@ -98,7 +49,8 @@ for row in table.find_all("tr"):
   cell = row.find_all("td")
   if ((len(cell) == 6)):
     RATING.append(cell[0].text.strip())
-    DATE.append(cell[1].text.strip())
+    DATE.append(parse(cell[1].text.strip()).strftime("%d, %b %Y"))
+
     SHOW.append(cell[2].text.lstrip().replace('\n',':'))
     if (randint(0,1)):
       match = cell[3].text.lstrip() + "vs " + cell[4].text.lstrip()
@@ -106,28 +58,20 @@ for row in table.find_all("tr"):
       match = cell[4].text.lstrip() + "vs " + cell[3].text.lstrip()
     MATCH.append(match)
 
-
 df = pd.DataFrame(RATING)
 df["DATE"]=DATE
 df["SHOW"]=SHOW
 df["MATCH"]=MATCH
-print(df["DATE"])
-# print(df.sort("DATE"))
-# df.to_csv("topMatches.csv", index=False, header=False)
+df.insert(0,"CHECK",'[ ]')
+
+# Save the sorted by ranking
+# sortTopMatches(df)
+# Save the sorted by date list
+# sortByDate(df)
+print("Fin")
 
 
-  # print("------")
-  # print("F")
-  # print(row.text)
-
-
-# for match in matches:
-  # print(match.text)
   # print("========")
     # movie += ' ('
     # if movie in title.text:
     #     print("==",title.text)
-
-
-
-print("Fin")

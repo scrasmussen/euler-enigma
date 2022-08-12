5975b1c7d2b8681831f6835aa2b6f6dfc8490e96
Soren Rasmussen <s.c.rasmussen@gmail.com>
2022-08-11 22:18:03 -0700

    faster

--- project-euler/jl_mods/sieveOfEratosthenes.jl
+++ project-euler/jl_mods/sieveOfEratosthenes.jl
@@ -4,21 +4,37 @@ module sieveOfEratosthenes
 export sieveOE_init, isPrime
 
 function sieveOE_init(n)
-    A = ones(Int32,n)
-    A[1] = 0
-    for i in 2:n
-        for j in i*2:i:n
-            A[j] = 0
+    if (mod(n,2) == 1)
+        n = n + 1
+    end
+    n2 = Int32(n/2)
+    A = ones(Int32,n2)
+
+    for i in 1:n2
+        if (A[i] == 1)
+            for j in i+i*2+1:i*2+1:n2
+                A[j] = 0
+            end
         end
     end
     return A
 end
 
-function isPrime(i, A)
-    if (A[i] == 0)
+function isPrime(i::Int, A)
+    if (i == 2)
+        return true
+    elseif (i == 1 || mod(i,2) == 0)
+        return false
+    end
+
+    i2 = trunc(Int,(i-1) / 2)
+
+
+    if (A[i2] == 0)
         return false
     else
         return true
     end
 end
-end
+
+end # module
